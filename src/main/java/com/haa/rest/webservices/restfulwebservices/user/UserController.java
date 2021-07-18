83ec3dedd7454cfbdbba2c85f3bc906eab25d1bf
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-07-18 20:40:36 +0530

    create new user with correct resp code - 201 Created

--- src/main/java/com/haa/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/haa/rest/webservices/restfulwebservices/user/UserController.java
@@ -1,13 +1,16 @@
 package com.haa.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -25,9 +28,21 @@ public class UserController {
         return userService.findUserOne(id);
     }
 
-    @PostMapping(path = "/users")
-    public void createUser(@RequestBody User user) {
+    // Creats new user with response code - 200 OK
+    @PostMapping(path = "/users-create")
+    public void createUserOld(@RequestBody User user) {
         userService.saveUser(user);
+    }
+
+    // Best Practice - Create new user with correct response code - 201 Created
+    @PostMapping(path = "/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User saveUser = userService.saveUser(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(saveUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
