389b648c174c9aee7b7a9bdbb895cff83cb412dd
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-07-27 18:57:20 +0530

    internationalizing services my configuring messages property

--- src/main/java/com/haa/rest/webservices/restfulwebservices/hello/HelloWorldController.java
+++ src/main/java/com/haa/rest/webservices/restfulwebservices/hello/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.haa.rest.webservices.restfulwebservices.hello;
 
+import java.util.Locale;
+
+import org.apache.logging.log4j.message.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     // @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
@@ -21,5 +30,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-var/{name}")
     public HelloWroldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWroldBean(String.format("Hello World, Welcome to %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalize")
+    public String helloWorldInternationalize(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
