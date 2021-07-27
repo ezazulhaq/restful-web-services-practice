b6e4298b09dc4243121fcabbf22f6951c6419d26
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-07-27 19:01:57 +0530

    simple way to internationalize without using RequestHeader

--- src/main/java/com/haa/rest/webservices/restfulwebservices/hello/HelloWorldController.java
+++ src/main/java/com/haa/rest/webservices/restfulwebservices/hello/HelloWorldController.java
@@ -2,9 +2,9 @@ package com.haa.rest.webservices.restfulwebservices.hello;
 
 import java.util.Locale;
 
-import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -33,8 +33,11 @@ public class HelloWorldController {
     }
 
     @GetMapping(path = "/hello-world-internationalize")
-    public String helloWorldInternationalize(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+    public String helloWorldInternationalize() {
+        // public String helloWorldInternationalize(@RequestHeader(name =
+        // "Accept-Language", required = false) Locale locale) {
 
-        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+        return messageSource.getMessage("good.morning.message", null, "Default Message",
+                LocaleContextHolder.getLocale());
     }
 }
