94ae2098cd44608f2f373df09c1e854d63879917
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-07-27 23:56:37 +0530

    Static filtering of Json - method 2

--- src/main/java/com/haa/rest/webservices/restfulwebservices/filter/SomeBean.java
+++ src/main/java/com/haa/rest/webservices/restfulwebservices/filter/SomeBean.java
@@ -1,15 +1,14 @@
 package com.haa.rest.webservices.restfulwebservices.filter;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(value = { "field1", "field2" })
 public class SomeBean {
 
     private String field1;
 
-    @JsonIgnore
     private String field2;
 
-    @JsonIgnore
     private String field3;
 
     public SomeBean(String field1, String field2, String field3) {
