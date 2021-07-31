ed5b9057316efddb878af1dc1e405ad6ebb2b430
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-07-31 14:13:45 +0530

    refractoring person controller

--- src/main/java/com/haa/rest/webservices/restfulwebservices/person/PersonContoller.java
+++ src/main/java/com/haa/rest/webservices/restfulwebservices/person/PersonContoller.java
@@ -6,43 +6,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonContoller {
 
+    String firstName = "Ezazul Haq";
+    String lastName = "Abdul";
+
     @GetMapping(path = "/v1/person")
     public PersonV1 personV1() {
-        return new PersonV1("Ezazul Haq");
+        return new PersonV1(firstName);
     }
 
     @GetMapping(path = "/v2/person")
     public PersonV2 personV2() {
-        return new PersonV2(new Name("Ezazul Haq", "Abdul"));
+        return new PersonV2(new Name(firstName, lastName));
     }
 
     @GetMapping(value = "/person/param", params = "version=1")
     public PersonV1 paramV1() {
-        return new PersonV1("Ezazul Haq");
+        return new PersonV1(firstName);
     }
 
     @GetMapping(value = "/person/param", params = "version=2")
     public PersonV2 paramV2() {
-        return new PersonV2(new Name("Ezazul Haq", "Abdul"));
+        return new PersonV2(new Name(firstName, lastName));
     }
 
     @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
     public PersonV1 headerV1() {
-        return new PersonV1("Ezazul Haq");
+        return new PersonV1(firstName);
     }
 
     @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
     public PersonV2 headerV2() {
-        return new PersonV2(new Name("Ezazul Haq", "Abdul"));
+        return new PersonV2(new Name(firstName, lastName));
     }
 
     @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
     public PersonV1 producesV1() {
-        return new PersonV1("Ezazul Haq");
+        return new PersonV1(firstName);
     }
 
     @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
     public PersonV2 producesV2() {
-        return new PersonV2(new Name("Ezazul Haq", "Abdul"));
+        return new PersonV2(new Name(firstName, lastName));
     }
 }
